d5eb7fcb9f666cd5a5bb166cdf1a913dae3a741d
Ruud Schroen <ruud@infoklik.nl>
2018-06-21 15:10:26 +0200

    Added python template

---
+++ nxstart/templates/python/main.py
@@ -0,0 +1,21 @@
+import nx
+
+"""
+    Main file for APP_NAME_PLACEHOLDER
+
+    File name: main.py
+    Author: APP_AUTHOR_PLACEHOLDER
+    Date created: DATE_PLACEHOLDER
+    Date last modified: DATE_PLACEHOLDER
+"""
+
+
+def main():
+    """
+    The main loop
+    """
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
